21fcbcb76302750d431b5f081f2c875d1422ec71
Kuklyshyn <kuklishinnick@gmail.com>
2020-11-06 17:20:10 +0200

    init

--- src/index.js
+++ src/index.js
@@ -11,8 +11,9 @@ import {rootReducer} from "./redux/rootReducer";
 const store = createStore(rootReducer, compose(
     applyMiddleware(
         thunk
-    ),
-    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+    )
+    // ,
+    // window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
 ));
 const app = (
     <Provider store={store}>
